2b11b75a74fd6356d7afdfbeac39bcea71e30300
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-04-17 20:47:25 +0530

    initialize project

---
+++ AutomatedSS/AutomatedSS/main.c
@@ -0,0 +1,18 @@
+/*
+ * AutomatedSS.c
+ *
+ * Created: 4/17/2022 8:38:56 PM
+ * Author : bimal
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
