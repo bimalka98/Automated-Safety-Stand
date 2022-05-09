fc3d1e777d087268aad2e45a53f3193bb050e89a
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-05-09 22:30:17 +0530

    add read pitch angle from mpu using the polling mode

--- Arduino/automatic_safety_stand/automatic_safety_stand.ino
+++ Arduino/automatic_safety_stand/automatic_safety_stand.ino
@@ -1,4 +1,5 @@
-/* ============================================
+/* Copyright notice for MPU6050 lib by Jeff Rowberg
+  ============================================
   I2Cdev device library code is placed under the MIT license
   Copyright (c) 2012 Jeff Rowberg
 
@@ -22,101 +23,123 @@
   ===============================================
 */
 
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
+// including I2Cdev and MPU6050 libraries
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h" //"MPU6050.h" is included in this header file
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
+/* class default I2C address is 0x68
+  specific I2C addresses may be passed as a parameter here
+  AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+  AD0 high = 0x69 */
 MPU6050 mpu; // creating an object of the class MPU6050
-//MPU6050 mpu(0x69); // <-- use for AD0 high
+// MPU6050 mpu(0x69); // <-- use for AD0 high
 
-
-/* =========================================================================
-   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-   depends on the MPU-6050's INT pin being connected to the Arduino's
-   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-   digital I/O pin 2.
-   ========================================================================= */
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
-
-// define "OUTPUT_READABLE_YAWPITCHROLL" to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+/* define "OUTPUT_READABLE_YAWPITCHROLL" to see the yaw/
+  pitch/roll angles (in degrees) calculated from the quaternions coming
+  from the FIFO. Note this also requires gravity vector calculations.
+  Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+  more info, see: http://en.wikipedia.org/wiki/Gimbal_lock) */
 #define OUTPUT_READABLE_YAWPITCHROLL
 
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+// *****************************************************************************
+/* *********************   VARIABLE DECLARATION    *********************      */
+// *****************************************************************************
 
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+/* MPU control/status vars */
+bool dmpReady = false;        // set true if DMP init was successful
+uint8_t mpuIntStatus;         // holds actual interrupt status byte from MPU
+uint8_t devStatus;            // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;          // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;           // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];       // FIFO storage buffer
+uint8_t calibrationLoops = 6; // calibration loops
+
+/* orientation/motion vars */
+Quaternion q;                 // [w, x, y, z]         quaternion container
+VectorInt16 aa;               // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;           // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;          // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;          // [x, y, z]            gravity vector
+float euler[3];               // [psi, theta, phi]    Euler angle container
+float ypr[3];                 // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+/* program specific variables */
+float setAngleOfBike = 0; // set angle of bike: when it is in rest position
 
 
+// *****************************************************************************
+/* **************      USER DEFINED FUNCTIONS DECLARATION **************      */
+// *****************************************************************************
 
-// ================================================================
-// ===               mpu6050 imu  initialization                ===
-// ================================================================
+void initializeMPU6050();       // fucntion to initialize the MPU6050
+float getCurrentangleOfBike();  // function to get the current angle of the bike
 
-void initializeMPU6050(){
-  
+// *****************************************************************************
+/* **************     SET UP FUCNTION RUNS ONCE AT STARTUP **************      */
+// *****************************************************************************
+
+void setup() { // setup function runs once at startupS
+
   // join I2C bus (I2Cdev library doesn't do this automatically)
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-  #endif
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  // initialization of MPU6050 IMU
+  initializeMPU6050();
+
+}
+
+// *****************************************************************************
+/* *****************     LOOP FUCNTION RUNS REPEATEDLY *****************      */
+// *****************************************************************************
+
+void loop() { // loop function runs repeatedly
+
+  // get current angle of the bike
+  float _currentangle = getCurrentangleOfBike();
+
+}
+
+// *****************************************************************************
+/* **************      USER DEFINED FUNCTIONS DEFINITIONS **************      */
+// *****************************************************************************
+
+// fucntion to initialize the MPU6050
+void initializeMPU6050() {
 
   // initialize device
   mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
+  //pinMode(INTERRUPT_PIN, INPUT);
 
   // load and configure the DMP : internal digital motion processor
-  /*MPU6050’s onboard processor that combines the 
-  data coming from the accelerometer and gyroscope.*/
   devStatus = mpu.dmpInitialize();
 
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+  // set accelerometer and gyroscope offsets to zero
+  mpu.setXAccelOffset(0); mpu.setYAccelOffset(0); mpu.setZAccelOffset(0);
+  mpu.setXGyroOffset(0); mpu.setYGyroOffset(0); mpu.setZGyroOffset(0);
 
 
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
     // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
+    mpu.CalibrateAccel(calibrationLoops);
+    mpu.CalibrateGyro(calibrationLoops);
 
     // turn on the DMP, now that it's ready
     mpu.setDMPEnabled(true);
 
     // enable Arduino interrupt detection
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
+    //attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    //mpuIntStatus = mpu.getIntStatus();
 
     // set our DMP Ready flag so the main loop() function knows it's okay to use it
     dmpReady = true;
@@ -124,51 +147,44 @@ void initializeMPU6050(){
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
 
-  } else {
-    //[TODO] should implement something to handle this: some debug output or something
-
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    //        Serial.print(F("DMP Initialization failed (code "));
-    //        Serial.print(devStatus);
-    //        Serial.println(F(")"));
   }
-}
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
-
-
-void setup() {
-  
-  // initialization of MPU6050 IMU
-  initializeMPU6050();
 
 }
 
-void loop() {
-  // if programming failed, don't try to do anything
-  if (!dmpReady) return;
+// function to get the current angle of the bike
+float getCurrentangleOfBike() {
+
+  // doble check if MPU6050 is ready
+  if (!dmpReady) {
+    initializeMPU6050(); // try to initialize again
+  }
+
+  // clear the buffer to take the new data: https://mjwhite8119.github.io/Robots/mpu6050
+  mpu.resetFIFO();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // wait for correct available data length, should be a VERY short wait
+  while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+  // declaring variables to hold yaw, pitch, roll
+  float _yaw, _pitch, _roll;
+
   // read a packet from FIFO
   if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
-//    #ifdef OUTPUT_READABLE_YAWPITCHROLL
-    // display Euler angles in degrees
+
+    // calculate Euler angles in degrees
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    float yaw = ypr[0] * 180/M_PI;
-    float pitch = ypr[1] * 180/M_PI;
-    float roll = ypr[2] * 180/M_PI;
-//    #endif
+
+    // calcuate ya/pitch/roll angles
+    _yaw = ypr[0] * 180 / M_PI; // Yaw angle: rotation around the z-axis
+    _pitch = ypr[1] * 180 / M_PI; // Pitch angle: rotation around the y-axis
+    _roll = ypr[2] * 180 / M_PI; // Roll angle: rotation around the x-axis
   }
 
-
+  // return the angle: rotation around y-axis
+  return _pitch;
 }
